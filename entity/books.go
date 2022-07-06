c4e394d98a8ee5455b8a7f4635ed8e8689d47363
muhdjau <mhmd.jauhari@gmail.com>
2022-07-06 23:20:11 +0700

    delete field Content_book

--- entity/books.go
+++ entity/books.go
@@ -4,9 +4,8 @@ import "gorm.io/gorm"
 
 type Books struct {
 	gorm.Model
-	Name_book    string
-	Content_book string
-	User_id      uint
+	Name_book string
+	User_id   uint
 }
 
 type AksesBooks struct {
